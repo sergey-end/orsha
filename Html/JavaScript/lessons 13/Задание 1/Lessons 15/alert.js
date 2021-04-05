a1f6724d427ad1093a318fce6c8eb7a80b4394ae
sergey-end <13optimist@mail.ru>
2021-04-05 20:13:14 +0300

    ededede

--- Html/JavaScript/lessons 13/Задание 1/Lessons 15/alert.js
+++ Html/JavaScript/lessons 13/Задание 1/Lessons 15/alert.js
@@ -1,3 +1,5 @@
 "use srtict";
 const element = document.querySelectorAll('div');
 console.log(element[0].innerHTML);
+console.log(element[0].afterHTML);
+console.log(element[0].innerHTML +=  "Простота- зплог надежности");
